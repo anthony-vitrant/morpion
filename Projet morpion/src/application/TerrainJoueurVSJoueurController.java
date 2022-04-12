bd0cc4be6641a7f08047e88b3b1bc19771fbd76d
Annox <anthony.vitrant@orange.fr>
2022-04-13 00:57:20 +0200

    add transparency to whole design

--- Projet morpion/src/application/TerrainJoueurVSJoueurController.java
+++ Projet morpion/src/application/TerrainJoueurVSJoueurController.java
@@ -124,15 +124,15 @@ public class TerrainJoueurVSJoueurController {
     public void setPlayerSymbol(Button button){
         if(playerTurn % 2 == 0){
             button.setText("X");
-            button.setTextFill(Color.BLUE);
-            button.setStyle("-fx-opacity: 1");
+            button.setTextFill(Color.BLACK);
+            button.setStyle("-fx-opacity: 0.7");
             button.setFont(Font.font("Arial", FontWeight.BOLD, 40));
             playerTurn = 1;
         }
         else{
         	button.setText("O");
-            button.setTextFill(Color.RED);
-            button.setStyle("-fx-opacity: 1");
+            button.setTextFill(Color.PURPLE);
+            button.setStyle("-fx-opacity: 0.7");
             button.setFont(Font.font("Arial", FontWeight.BOLD, 40));
             playerTurn = 0;
         }
@@ -226,7 +226,7 @@ public class TerrainJoueurVSJoueurController {
     public void disableAll() { // désactivation de tous les bouttons
     	buttons.forEach(button ->{
             button.setDisable(true);
-            button.setStyle("-fx-opacity: 1");
+            button.setStyle("-fx-opacity: 0.7");
         });
     }
 }
